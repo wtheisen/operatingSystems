b4e8822d6ebb5b1e5ad27ceb61e762650cfe9d9a
michaelburke1 <michaeljburke18@gmail.com>
2017-03-19 14:02:07 -0400

    initial commit mburke. fetching url

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <string>
+#include <curl/curl.h>
+
 
 using namespace std;
+
+vector<String> getURLString(string url) 
+{
+    
+}
 
 int main(int argc, char *argv[])
 {
